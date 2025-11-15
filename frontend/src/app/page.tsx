885b93f8c570c632e29b69f2a1984a7dc207a6ac
uyenpham139 <uyenpham47139@gmail.com>
2025-11-15 15:23:34 +0700

    Update path when user logged in and not logged in yet

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from 'next/image'
 import Link from 'next/link';
 
@@ -7,19 +9,23 @@ import Plane from '@/assets/images/paper-plane.png';
 import ExampleImage from '@/assets/images/example-image.jpg';
 
 import { DoneOutlined, InfoOutlined, CheckCircleOutline } from '@mui/icons-material';
+import { useAuth } from '@/contexts/AuthContext';
+
+const howToWorkTexts = [
+  {title: "Tell us your theme", description: 'Want a "Historical Food Tour"? Just give our AI a theme. Our AI understands context and builds a logical plan.'},
+  {title: "Get your AI Plan", description: 'Instantly receive a full-day, multi-stop itinerary. We find the locations, calculate optimized routes (walking, bus, ect.), and show it all on one map.'},
+  {title: "Refine & Personalize", description: "Want to add a cool café? Type in its name, or upload a photo – our AI will find it and add it to your route instantly. You're in control"},
+];
+
+const exploreTexts = [
+  {text: "View destinations with NAVER's 3D aerial map."},
+  {text: "Get AI-powered summaries of history and travel tips."},
+  {text: "See real-time public transit info for your route."},
+]
 
 export default function Home() {
 
-  const howToWorkTexts = [
-    {id: 1, title: "Tell us your theme", description: 'Want a "Historical Food Tour"? Just give our AI a theme. Our AI understands context and builds a logical plan.'},
-    {id: 2, title: "Get your AI Plan", description: 'Instantly receive a full-day, multi-stop itinerary. We find the locations, calculate optimized routes (walking, bus, ect.), and show it all on one map.'},
-    {id: 3, title: "Refine & Personalize", description: "Want to add a cool café? Type in its name, or upload a photo – our AI will find it and add it to your route instantly. You're in control"},
-  ];
-  const exploreTexts = [
-    {id: 1, text: "View destinations with NAVER's 3D aerial map."},
-    {id: 2, text: "Get AI-powered summaries of history and travel tips."},
-    {id: 3, text: "See real-time public transit info for your route."},
-  ]
+  const { userLoggedIn } = useAuth();
 
   return (
     <div className='w-full min-h-screen'>
@@ -42,7 +48,7 @@ export default function Home() {
           </p>
           <div className='mt-10 flex flex-col sm:flex-row sm:items-center sm:gap-6'>
             <Link 
-              href={`/plan`} 
+              href={`${userLoggedIn ? '/plan' : '/auth/login'}`} 
               className='inline-block header-h5 px-[46px] py-5 bg-secondary text-light-text rounded-[8px] hover:bg-[color-mix(in_srgb,var(--color-secondary),black_10%)] transition'
             >
               Generate
@@ -89,10 +95,10 @@ export default function Home() {
         <p className='paragraph-p1-semibold text-sub-text text-center'>Stop juggling 5 different apps. Travista unifies your entire trip.</p>
 
         <div className='w-full flex flex-col gap-12 lg:flex-row lg:gap-0 justify-around my-[102px]'>
-          {howToWorkTexts.map(text => (
-            <div key={text.id} className='w-full px-12 lg:w-[368px] flex flex-col justify-start items-center'>
+          {howToWorkTexts.map((text, index) => (
+            <div key={index} className='w-full px-12 lg:w-[368px] flex flex-col justify-start items-center'>
               <div className='w-[86px] h-[86px] rounded-full bg-primary text-light-text header-h4 flex justify-center items-center'>
-                {text.id}
+                {index + 1}
               </div>
               <p className='header-h5 text-dark-text mt-[33px]'>
                 {text.title}
@@ -121,8 +127,8 @@ export default function Home() {
           <h2 className='header-h2 text-dark-text'>Explore with Street View. <br/> Understand with AI</h2>
           <p className='paragraph-p1-semibold text-sub-text pr-2'>Our split-screen interface connects NAVER's Map and Street View with our AI's insights. <br/> Don't just see where you're going - understand why it's amazing.</p>
           <div className='mt-4'>
-            {exploreTexts.map(text => (
-              <div key={text.id} className='mt-4 flex gap-2'>
+            {exploreTexts.map((text, index) => (
+              <div key={index} className='mt-4 flex gap-2'>
                 <div className='w-[30px] h-[30px] rounded-full bg-secondary text-light-text header-h4 flex justify-center items-center'>
                   <DoneOutlined />
                 </div>
@@ -151,7 +157,7 @@ export default function Home() {
         <h2 className='header-h2 text-light-text'>Start creating your itinerary now</h2>
         <p className='paragraph-p1-semibold text-light-text mt-8 mb-[70px]'>Your next adventure in Korea is just one click away.</p>
         <Link 
-          href={'/plan'}
+          href={`${userLoggedIn ? '/plan' : '/auth/login'}`}
           className='bg-secondary text-light-text paragraph-p1-semibold px-6 py-4 rounded-[8px] hover:bg-[color-mix(in_srgb,var(--color-secondary),black_10%)] transition'
         >
           Generate your first plan
