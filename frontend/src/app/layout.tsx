beed2cf0ac34d0ffec8e49396a4e8e500eaa8be7
uyenpham139 <uyenpham47139@gmail.com>
2025-11-19 17:49:15 +0700

    Update language english for map

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <head>
         <Script 
           strategy='afterInteractive'
-          src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpKeyId=${process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID}&submodules=panorama,geometry`}
+          src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpKeyId=${process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID}&submodules=panorama,geometry&language=en`}
         />
       </head>
       <body className={`${inter.variable} antialiased`}>
